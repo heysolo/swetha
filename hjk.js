1d19c38d3f7af13ca07e70410ef545fa8e996e4b
heysolo <playersolo809@gmail.com>
2025-12-26 13:20:03 +0530

    Add files via upload

---
+++ hjk.js
@@ -0,0 +1,53 @@
+function crt()
+{
+    let a=document.getElementById("val1")
+    a=a.value
+    let b=document.getElementById("val2")
+    b=b.value
+    let c=document.getElementById("val3")
+    c=c.value
+    let d=document.getElementById("val4")
+    d=d.value
+    let e=document.getElementById("val5")
+    e=e.value
+    let f=document.getElementById("val6")
+    f=f.value
+    let g=document.getElementById("val7")
+    g=g.value
+    let h=document.getElementById("val8")
+    h=h.value
+    let i=document.getElementById("val9")
+    i=i.value
+    let j=document.getElementById("val10")
+    j=j.value
+    let k=document.getElementById("val11")
+    k=k.value
+    let l=document.getElementById("val12")
+    l=l.value
+
+    let name=document.getElementById("name")
+    name.innerText=a
+    let phonenumber=document.getElementById("phonenumber")
+    phonenumber.innerText=b
+    let gmail=document.getElementById("gmail")
+    gmail.innerText=c
+    let address=document.getElementById("address")
+    address.innerText=d
+    let college=document.getElementById("college")
+    college.innerText=e
+    let school=document.getElementById("school")
+    school.innerText=f
+    let skill=document.getElementById("skills")
+    skill.innerText=g
+    let language=document.getElementById("language")
+    language.innerText=h
+    let summary=document.getElementById("summar")
+    summary.innerText=i
+    let experenes=document.getElementById("experenes")
+    experenes.innerText=j
+    let start=document.getElementById("start")
+    start.innerText=k
+    let end=document.getElementById("end")
+    end.innerText=l
+    
+}
